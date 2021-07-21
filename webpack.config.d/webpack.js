4c6ecc72cc6fc9103c80d4d7000296fadbe39ef2
Robert Jaros <rjaros@finn.pl>
2021-07-21 20:30:18 +0200

    source maps

--- webpack.config.d/webpack.js
+++ webpack.config.d/webpack.js
@@ -2,7 +2,6 @@ config.resolve.modules.push("../../processedResources/js/main");
 
 if (config.devServer) {
     config.devServer.hot = true;
-    config.devtool = 'eval-cheap-source-map';
 } else {
     config.devtool = undefined;
 }
